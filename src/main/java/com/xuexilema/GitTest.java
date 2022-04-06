ca9d7523552d8b0637828d78df838bd22a5ff2ae
zhuaxiaowa <101301962+zhuaxiaowa@users.noreply.github.com>
2022-04-06 22:20:59 +0800

    thrid commit

--- src/main/java/com/xuexilema/GitTest.java
+++ src/main/java/com/xuexilema/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("你好！");
     }
 }
